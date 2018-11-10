684bda82b6ced78ddaa93b8ae3efcae9622bcd94
patrickcunha <patrick.cunha336@gmail.com>
2018-11-10 02:22:06 -0300

    initial commit

---
+++ src/services/api.js
@@ -0,0 +1,7 @@
+import apisauce from 'apisauce'
+
+export default apisauce.create({
+    //baseURL:"http://10.0.2.2:3000" //Emulador Android SDK Padrao
+    //baseURL:"http://10.0.3.2:3000" //Emulador GeniMotion
+    baseURL:"http://localhost:3000" //Emulador IOS
+})
